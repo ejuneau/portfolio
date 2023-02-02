f122b7fed0e1d5a2da49396e9cf38e85aba9d881
ejuneau <rcjuneau8@gmail.com>
2023-02-02 18:14:08 -0500

    initial Commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header(props) {
+  return (
+    <header className="Header" style={{"backgroundColor": props.backgroundColor}}>
+      <h1><a href="./">EJ</a></h1>
+      <nav>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About</a></li>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
