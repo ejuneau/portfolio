eb0c9b19e63317a01127630e75a64041017fbf31
ejuneau <rcjuneau8@gmail.com>
2023-02-03 12:56:17 -0500

    Added About Modal by using react-modal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import About from './About';
+import Modal from 'react-modal';
+
 
 const offBlack = "#0F0F0F";
 const jammmingColor = '#010c3f';
 const artemisColor = '#415a74';
 const ravenousColor = '#cca353';
 
+const modalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: offBlack,
+    position: 'absolute',
+    border: 'none',
+  },
+};
 
 function App() {
 
@@ -15,7 +31,7 @@ function App() {
   const [backgroundColor, setBackgroundColor] = useState(offBlack);
  
   useEffect(() => {
-    console.log("effect");
+    console.log(`Changing Background ${backgroundColor === offBlack ? 'back ': ''}to ${backgroundColor}...`);
   }, [backgroundColor]);
  
   const artemisHandler = () => {
@@ -31,13 +47,33 @@ function App() {
     setBackgroundColor(offBlack);
   }
 
+  const [modalIsOpen, setIsOpen] = useState(false);
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  Modal.setAppElement('#root')
 
   return (
     <>
-    <Header backgroundColor={backgroundColor}/>
+    <Header backgroundColor={backgroundColor} openModal={openModal} />
     <div className="App" style={{"backgroundColor": backgroundColor}}>
-        <ul className="Portfolio">
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="About Modal"
+        style={modalStyles}
+        overlayClassName="ModalOverlay"
+        closeTimeoutMS={200}
+      >
+        <About closeModal={closeModal} />
+      </Modal>
+        <ul className="Portfolio" id="Portfolio">
           <li onMouseEnter={artemisHandler} onMouseLeave={exitHandler}><a href="https://ejuneau.github.io/CA_Artemis/" target="_blank" rel="noreferrer">Artemis for Reddit</a></li>
           <li onMouseEnter={jammmingHandler} onMouseLeave={exitHandler}><a href="https://ejuneau.github.io/CA_Jammming/" target="_blank" rel="noreferrer">Jammming for Spotify</a></li>
           <li onMouseEnter={ravenousHandler} onMouseLeave={exitHandler}><a href="https://ejuneau.github.io/CA_Ravenous/" target="_blank" rel="noreferrer">Ravenous for Yelp</a></li>
