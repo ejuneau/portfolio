9f025204da7076e29242703defbb42c9d054a589
Eve <rcjuneau8@gmail.com>
2023-11-14 12:09:20 -0500

    Added responsive text, hamburger menu on mobile

--- src/About.js
+++ src/About.js
@@ -3,8 +3,8 @@ import './About.css';
 function About(props) {
   return (
     <div className="AboutContainer">
-      <p className="About">EJ is a UI and UX designer based in Montreal, Canada specializing in web design via React.</p>
-      <p className="CloseModal" onClick={props.closeModal}>Close</p>
+      <p className="About">My name is Eve Juneau (they/she), a UI and UX designer based in Montreal, Canada. Specializing in web design via React, I'm passionate about creating user experiences that stick with people.</p>
+      <p className="CloseModal" onClick={props.closeModal}>close</p>
     </div>
   );
 }
