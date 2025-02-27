cc5e704d6cb14847c4cbf918acd9e91d9abd6e1c
Eve <rcjuneau8@gmail.com>
2025-02-27 10:45:16 -0500

    Updated site descriptions

--- src/Assets/sites.js
+++ src/Assets/sites.js
@@ -67,12 +67,12 @@ const sites = [
         key:                "spyglass",
         link:               "https://spyglass-hsef.onrender.com/",
         title:              "Spyglass Eyewear",
-        description:        "Sunglasses webstore.",
+        description:        "Eyeglasses webstore.",
         desktopImageFull:   spyglassDesktopFull,
         desktopImagePh:     spyglassDesktopPh, 
         mobileImageFull:    spyglassMobileFull,
         mobileImagePh:      spyglassMobilePh,
-        longDescription:    `Spyglass Eyewear is a portfolio project designed by me. Everything about this project was done by myself, from from designing the layout to hosting the website and even to coordinating the photoshoots for the glasses models. The goal of this project was to design a theoretical webstore with a striking visual design that immediately grabs the client's attention - animations are flashy, colours are bright, everything about the site is meant to dazzle. The off-centre design proved to be a very fun challenge to achieve in CSS, and the final result speaks for itself. The design of this website is heavily inspired by Persona 5, a video game with a similar visual identity.`,
+        longDescription:    `Spyglass Eyewear is a portfolio project designed by myself. Everything about this project was done by myself, from from designing the layout to hosting the website and even to coordinating the photoshoots for the glasses models. The goal of this project was to design a theoretical webstore with a striking visual design that immediately grabs the client's attention - animations are flashy, colours are bright, everything about the site is meant to dazzle. The off-centre design proved to be a very fun challenge to achieve in CSS, and the final result speaks for itself. The design of this website is heavily inspired by Persona 5, a video game with a similar visual identity.`,
         involvement:        [
                                 "Full website design and development", 
                                 "Website built in React.js and hosted on Render", 
@@ -105,7 +105,7 @@ const sites = [
         desktopImagePh:     soupgangDesktopPh, 
         mobileImageFull:    soupgangMobileFull,
         mobileImagePh:      soupgangMobilePh,
-        longDescription:    "Openkitchen is a portfolio project by me, with the main pitch of its idea being a centralised, crowd-funded meal service. The goal of this project was to showcase the valuable skills and lessons I have learned completing my certification for Full-Stack development. Combining my design experience with the suite of services offered by Google's Firebase platform, I was able to create a fully functional database to manage users and their credentials within this project.",
+        longDescription:    "Openkitchen is a portfolio project by myself, with the main pitch of its idea being a centralised, crowd-funded meal service. The goal of this project was to showcase the valuable skills and lessons I have learned completing my certification for Full-Stack development. Combining my design experience with the suite of services offered by Google's Firebase platform, I was able to create a fully functional database to manage users and their credentials within this project.",
         involvement:        [
                                 "Full website design and development", 
                                 "Website built in React.js and hosted on Render", 
